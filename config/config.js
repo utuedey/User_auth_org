f22b14d71aa1ba28a55b464a9d3fd12de797857f
utuedey <utuedeyejosph@gmail.com>
2024-07-05 22:45:25 +0100

    configure config.js and db.js

---
+++ config/config.js
@@ -0,0 +1,12 @@
+// sequelization configuration
+
+module.exports = {
+    development: {
+        url: process.env.DATABASE_URL,
+        dialect: 'postgres',
+    },
+    production: {
+        url: process.env.DATABASE_URL,
+        dialect: 'postgres',
+    },
+}
---
+++ config/db.js
@@ -0,0 +1,12 @@
+// Setup sequelize and connect to the database
+const { Sequelize } = require('sequelize');
+const config = require('./config');
+
+const env = process.env.NODE_ENV || 'development';
+const dbConfig = config[env];
+
+const sequelize = new Sequelize(dbConfig.url, {
+    dialect: dbConfig.dialect,
+});
+
+module.exports = sequelize;
